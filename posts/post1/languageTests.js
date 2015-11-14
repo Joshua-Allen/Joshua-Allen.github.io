dedddee5ff5657abc4d24c994784b6359e81a679
joshua allen <joshuaallen64@gmail.com>
2015-11-14 03:13:22 -0600

    draw stuff

--- posts/post1/languageTests.js
+++ posts/post1/languageTests.js
@@ -1,23 +1,40 @@
 // an array of all the posts as a url
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-setInterval(draw, 30);
+setInterval(render, 1000/60);
 
 //
-function draw()
+function render()
 {
-	ctx.clearRect(0, 0, 200, 200);
+	//ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
 
+	draw_point(200*Math.random(),200*Math.random())
+}
+
+function draw_line(x1, y1, x2, y2)
+{
 	ctx.beginPath();
-	ctx.moveTo(0,0);
-	ctx.lineTo(200,200*Math.random());
+	ctx.moveTo(x1,y1);
+	ctx.lineTo(x2,y2);
 	ctx.stroke();
 }
 
+function draw_circle(x, y, r)
+{
+	ctx.beginPath();
+	ctx.arc(x,y,r,0,2*Math.PI);
+	ctx.stroke();
+}
 
+function draw_rect(x,y,width,height)
+{
+	ctx.fillRect(x,y,width,height);
+}
 
-
-
+function draw_point(x,y)
+{
+	ctx.fillRect(x,y,1,1);
+}
 
 
 
