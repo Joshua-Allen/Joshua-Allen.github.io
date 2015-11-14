a6851b3973e53f766875f47bb5e763d56c14cf30
joshua allen <joshuaallen64@gmail.com>
2015-11-14 12:22:46 -0600

    how log works

--- posts/post1/languageTests.js
+++ posts/post1/languageTests.js
@@ -137,8 +137,7 @@ function render()
 	ctx_Astar = AstarCanvas.getContext("2d");
 	ctx_light = lightCanvas.getContext("2d");
 	
-	console.log("ctx_life = " + ctx_life);
-	console.log("ctx_life.width = " + ctx_life.width);
+	console.log("ctx_life = " + ctx_life + "  -  " + "ctx_life.width = " + ctx_life.width);
 	
 	//
 	ctx_life.clearRect(0, 0, lifeCanvas.width, lifeCanvas.height);
