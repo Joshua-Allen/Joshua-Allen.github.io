74db2789c6b4a8b1d72bb4c55d512b3520eb8ce8
joshua allen <joshuaallen64@gmail.com>
2015-11-14 02:43:40 -0600

    myCanvas

--- posts/post1/languageTests.js
+++ posts/post1/languageTests.js
@@ -1,5 +1,5 @@
 // an array of all the posts as a url
-var canvas = $("#canvas")[0];
+var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 setInterval(draw, 1000);
 
