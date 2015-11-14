b122d24931bf1f8db89839882af01c6c5a1ce3c6
joshua allen <joshuaallen64@gmail.com>
2015-11-14 02:51:05 -0600

    more to the draw

--- posts/post1/languageTests.js
+++ posts/post1/languageTests.js
@@ -1,11 +1,14 @@
 // an array of all the posts as a url
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-setInterval(draw, 1000);
+setInterval(draw, 1);
 
 //
 function draw()
 {
+	ctx.fillStyle = "#000000";
+	ctx.fillRect(0,0,200,200);
+
 	ctx.moveTo(0,0);
 	ctx.lineTo(200,200*Math.random());
 	ctx.stroke();
