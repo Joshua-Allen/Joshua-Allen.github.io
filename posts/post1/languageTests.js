4d786f02c65ecdd40468b07c51f3a68e1e2def99
joshua allen <joshuaallen64@gmail.com>
2015-11-14 11:33:23 -0600

    odd

--- posts/post1/languageTests.js
+++ posts/post1/languageTests.js
@@ -111,19 +111,26 @@ function render_life()
 //
 function render()
 {
-	console.log(5);
+	//console.log(5);
 	
 	//
 	ctx_life.clearRect(0, 0, lifeCanvas.width, lifeCanvas.height);
 	ctx_Astar.clearRect(0, 0, AstarCanvas.width, AstarCanvas.height);
 	ctx_light.clearRect(0, 0, lightCanvas.width, lightCanvas.height);
 	
+	current_context = ctx_life;
+	draw_line(0, 0, lifeCanvas.width, lifeCanvas.height);
+	current_context = ctx_Astar;
+	draw_line(0, 0, AstarCanvas.width, AstarCanvas.height);
+	current_context = ctx_light;
+	draw_line(0, 0, lightCanvas.width, lightCanvas.height);
+	
 	//
-	current_canvas = lifeCanvas;
-	lifeUpdate();
-	render_life();
+	//current_canvas = lifeCanvas;
+	//current_context = ctx_life;
 	
-	
+	//lifeUpdate();
+	//render_life();
 }
 
 
