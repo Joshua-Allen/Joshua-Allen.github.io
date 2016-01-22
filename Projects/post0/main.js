bc3f1bf954b612fdaa6b5a5339bf3eaebdaf9597
joshua allen <joshuaallen64@gmail.com>
2016-01-22 13:34:30 -0600

    big update

---
+++ Projects/post0/main.js
@@ -0,0 +1,99 @@
+//console.log(c);
+
+$(document).ready(function() {
+	set_page();
+});
+
+//
+function navClick(page) {
+	window.location.href = window.location.pathname + "?page=" + page;
+}
+
+// stuff to show the page
+function set_page() {
+	
+	// get the page var from the url
+	var pageVar = getQueryVariable("page");
+	
+	// get the real page location
+	var page = pageVar+"/index.html";
+	
+	//
+	var xmlhttp;
+	if (window.XMLHttpRequest) {
+		xmlhttp=new XMLHttpRequest();
+	} else {
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	
+	xmlhttp.onreadystatechange = function() {
+		// wait to get the post
+		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+			$("#mainSection").html(xmlhttp.responseText);
+		}
+	}
+	
+	xmlhttp.open("GET", page, true);
+	xmlhttp.send();
+}
+
+function getQueryVariable(variable) {
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+	   return "Home";
+}
+
+// stuff to fix the page
+//////////////////////////////////////////////////////////////////////////
+if (window.addEventListener) {
+    window.addEventListener("scroll", function() {fix_sidemenu();});
+    window.addEventListener("resize", function() {fix_sidemenu();});
+    window.addEventListener("touchmove", function() {fix_sidemenu();});
+    window.addEventListener("load", function() {fix_sidemenu();});
+} else if (window.attachEvent) {
+    window.attachEvent("onscroll", function() {fix_sidemenu();});
+    window.attachEvent("onresize", function() {fix_sidemenu();});
+    window.attachEvent("ontouchmove", function() {fix_sidemenu();});
+    window.attachEvent("onload", function() {fix_sidemenu();});
+}
+
+function fix_sidemenu() {
+	var w, top;
+	w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+	top = scrolltop()
+	if (w < 993 && w > 610) {
+		if (top > 82) {
+			document.getElementById("belowtopnav").style.paddingTop = "44px";
+			document.getElementById("topnav").style.position = "fixed";
+			document.getElementById("topnav").style.top = "0";
+		} else {
+			document.getElementById("belowtopnav").style.paddingTop = "0";
+			document.getElementById("topnav").style.position = "relative";
+		}
+	} else {
+		if (top > 82) {
+			document.getElementById("belowtopnav").style.paddingTop = "44px";
+			document.getElementById("topnav").style.position = "fixed";
+			document.getElementById("topnav").style.top = "0";
+		} else {
+			document.getElementById("belowtopnav").style.paddingTop = "0";
+			document.getElementById("topnav").style.position = "relative";
+		}
+	}
+}
+
+function scrolltop() {
+    var top = 0;
+    if (typeof (window.pageYOffset) == "number") {
+        top = window.pageYOffset;
+    } else if (document.body && document.body.scrollTop) {
+        top = document.body.scrollTop;
+    } else if (document.documentElement && document.documentElement.scrollTop) {
+        top = document.documentElement.scrollTop;
+    }
+    return top;
+}
