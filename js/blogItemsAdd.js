62bcae52ef774cf3ebd7427dc2d4a8363b73d360
joshua allen <joshuaallen64@gmail.com>
2016-01-23 10:42:18 -0600

    now

--- js/blogItemsAdd.js
+++ js/blogItemsAdd.js
@@ -29,11 +29,11 @@ function blog_set_posts(numberOfPosts) {
 		xmlhttp.onreadystatechange = function() {
 			// wait to get the post
 			if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-				posts[i+1] = blog_createPostSection(xmlhttp.responseXML);
+				posts[i] = blog_createPostSection(xmlhttp.responseXML);
 				
 				if ((posts.length-1) == numberOfPosts) {
 					var html = "";
-					for	(var index = 0; index < posts.length; index++) {
+					for	(var index = 1; index < posts.length; index++) {
 						html += posts[index];
 					}
 					$("#blogPostsSection").html(html);
