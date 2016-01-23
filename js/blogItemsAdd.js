40227456600ef0fc5f1fd6d2ccdf66a0edb9ff5e
joshua allen <joshuaallen64@gmail.com>
2016-01-23 09:42:35 -0600

    can now read lots of posts

--- js/blogItemsAdd.js
+++ js/blogItemsAdd.js
@@ -1,7 +1,7 @@
 //console.log(c);
 
 $(document).ready(function() {
-	set_posts();
+	set_posts(1);
 });
 
 //
@@ -10,30 +10,32 @@ function postClick(post) {
 }
 
 // load the posts xml
-function set_posts() {
-	var pageVar = "post0";
-	
-	// get the real page location
-	var xml = "Blog/posts/"+pageVar+"/info.xml";
-	
-	//
-	var xmlhttp;
-	if (window.XMLHttpRequest) {
-		xmlhttp=new XMLHttpRequest();
-	} else {
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	
-	xmlhttp.onreadystatechange = function() {
-		// wait to get the post
-		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-			//$("#mainSection").html(xmlhttp.responseText);
-			addPostSection(xmlhttp.responseText)
+function set_posts(numberOfPosts) {
+	for(var i=0; i<numberOfPosts; i++){
+		var pageVar = "post"+i;
+		
+		// get the real page location
+		var xml = "Blog/posts/"+pageVar+"/info.xml";
+		
+		//
+		var xmlhttp;
+		if (window.XMLHttpRequest) {
+			xmlhttp=new XMLHttpRequest();
+		} else {
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
 		}
+		
+		xmlhttp.onreadystatechange = function() {
+			// wait to get the post
+			if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+				//$("#mainSection").html(xmlhttp.responseText);
+				addPostSection(xmlhttp.responseXML)
+			}
+		}
+		
+		xmlhttp.open("GET", xml, true);
+		xmlhttp.send();
 	}
-	
-	xmlhttp.open("GET", xml, true);
-	xmlhttp.send();
 }
 
 // add the post to the site
