09b9ba204842112116b5128b3154ba6b2dfb8b7a
joshua allen <joshuaallen64@gmail.com>
2016-01-23 10:17:11 -0600

    adding the new html

--- js/blogItemsAdd.js
+++ js/blogItemsAdd.js
@@ -1,7 +1,7 @@
 //console.log(c);
 
 $(document).ready(function() {
-	//set_posts(1);
+	set_posts(1);
 });
 
 //
@@ -30,20 +30,62 @@ function blog_set_posts(numberOfPosts) {
 			// wait to get the post
 			if (xmlhttp.readyState==4 && xmlhttp.status==200) {
 				//$("#mainSection").html(xmlhttp.responseText);
-				posts[i] = createPostSection(xmlhttp.responseText);
+				posts[i] = createPostSection(xmlhttp.responseXML);
 			}
 		}
 		
 		xmlhttp.open("GET", xml, true);
 		xmlhttp.send();
 	}
+	
+	var html
+	for	(index = 0; index < posts.length; index++) {
+		html += fruits[index];
+	}
+	$("#blogPostsSection").html(html);
+	
 }
 
 // add the post to the site
 function blog_createPostSection(xml) {
 	var postHtml = "";
 	
-	postHtml = "";
+	postHtml += '<div class="w3-content w3-border-bottom">';
+	postHtml += '<div class="w3-row">';
+	
+	postHtml += '<div class="w3-col w3-container m4 l3">';
+	postHtml += '<p><canvas class="w3-border" id="tutorialsCanvas" width="150" height="150" style="width:100%"></canvas></p>';
+	postHtml += '</div>';
+	
+	postHtml += '<div class="w3-col w3-container m8 l9">';
+	
+	postHtml += '<p>text</p>';
+	
+	postHtml += '</div></div></div>';
+	
+	
 	console.log(postHtml);
 	return postHtml;
 }
+
+
+
+
+<div class="w3-content w3-border-bottom">
+	<div class="w3-row">
+		<div class="w3-col w3-container m4 l3">
+			<p><canvas class="w3-border" id="tutorialsCanvas" width="150" height="150" style="width:100%"></canvas></p>
+		</div>
+		<div class="w3-col w3-container m8 l9">
+			<p>
+			I love to teach, not only becuase it helps others learn but it also helps me understand the subject better. The tutorials Is where I will put things that I want other people to know.
+			</p>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
